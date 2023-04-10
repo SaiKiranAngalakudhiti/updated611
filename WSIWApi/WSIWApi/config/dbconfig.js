4f5a1652e18487bfa343b095c130068920fbf473
sangalak <sangalak@buffalo.edu>
2023-04-09 22:52:56 -0400

    uploading

---
+++ WSIWApi/WSIWApi/config/dbconfig.js
@@ -0,0 +1,10 @@
+const config = {
+    db: {
+      host: "localhost",
+      user: "root",
+      password: "Dm122448@",
+      database: "what_should_i_wear",
+      timezone: 'utc'
+    }
+  };
+  module.exports = config;
